20a2bc2fdac9ae3bbcc9133bf276c88c1b616177
Omnia Shehata Ahmed Eshra <113691665+OmniaEshra@users.noreply.github.com>
2023-03-30 22:41:20 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,113 @@
+<?php
+include_once('hms/include/config.php');
+if(isset($_POST['submit']))
+{
+$name=$_POST['fullname'];
+$email=$_POST['emailid'];
+$mobileno=$_POST['mobileno'];
+$dscrption=$_POST['description'];
+$query=mysqli_query($con,"insert into tblcontactus(fullname,email,contactno,message) value('$name','$email','$mobileno','$dscrption')");
+echo "<script>alert('The consultation has been sent successfully and will be answered to you as soon as possible.');</script>";
+echo "<script>window.location.href ='contact.php'</script>";
+
+}
+
+
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Medical Consultation</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css"  media="all" />
+		<link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<!--start-wrap-->
+		
+			<!--start-header-->
+			<div class="header">
+				<div class="wrap">
+				<!--start-logo-->
+				<div class="logo">
+		<a href="index.html" style="font-size: 30px;">Hospital Management System</a> 
+				</div>
+				<!--end-logo-->
+				<!--start-top-nav-->
+				<div class="top-nav">
+					<ul>
+						<li><a href="index.html">HomePage</a></li>
+					
+						<li class="active"><a href="contact.php">Medical Consultation</a></li>
+					</ul>					
+				</div>
+				<div class="clear"> </div>
+				<!--end-top-nav-->
+			</div>
+			<!--end-header-->
+		</div>
+		    <div class="clear"> </div>
+		   <div class="wrap">
+		   	<div class="contact">
+		   	<div class="section group">				
+				<div class="col span_1_of_3">
+					
+      			<div class="company_address">
+				     	<h2>Hospital Address:</h2>
+						    	<p>Egypt-Alexandria,</p>
+						   		<p>Sidi Gaber</p>
+						   		<p>Egypt</p>
+				   		<p>Phone No.:(00) 222 666 444</p>
+				   		<p>Landline Phone: (000) 000 00 00 0</p>
+				 	 	<p>E-mail: <span>info@mycompany.com</span></p>
+				   	
+				   </div>
+				</div>				
+				<div class="col span_2_of_3">
+				  <div class="contact-form">
+				  	<h2 style="text-align: center;">Write Your Consultation</h2>
+					    <form name="contactus" method="post">
+					    	<div>
+						    	<span><label>Name</label></span>
+						    	<span><input type="text" name="fullname" required="true" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>E-mail</label></span>
+						    	<span><input type="email" name="emailid" required="ture" value=""></span>
+						    </div>
+						    <div>
+						     	<span><label>Phone No.</label></span>
+						    	<span><input type="text" name="mobileno" required="true" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>Consultation</label></span>
+						    	<span><textarea name="description" required="true"> </textarea></span>
+						    </div>
+						   <div>
+						   		<span><input type="submit" name="submit" value="Send"></span>
+						  </div>
+					    </form>
+				    </div>
+  				</div>				
+			  </div>
+			  	 <div class="clear"> </div>
+	</div>
+	<div class="clear"> </div>
+			</div>
+	      <div class="clear"> </div>
+		   <div class="footer">
+		   	 <div class="wrap">
+		   	<div class="footer-r">
+		   			<ul>
+						<li><a href="index.html">HomePage</a></li>
+						
+						<li><a href="contact.php">Medical Consultation</a></li>
+					</ul>
+		   	</div>
+		  
+		   	<div class="clear"> </div>
+		   </div>
+		   </div>
+		<!--end-wrap-->
+	</body>
+</html>
+
